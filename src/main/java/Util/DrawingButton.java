90905a8bb8a6614b220461040067c34cfcb290f4
Selen <parlarselen@gmail.com>
2018-06-22 10:55:00 +0300

    Added comments for documentation.

--- src/main/java/Util/DrawingButton.java
+++ src/main/java/Util/DrawingButton.java
@@ -6,8 +6,17 @@ import java.net.URL;
 
 public class DrawingButton extends JButton {
 
+    /**
+     * A constructor of {@link DrawingButton} class.
+     *
+     * @param currentClass A {@link Class} type input.
+     * @param actionListener {@link ActionListener} type input.
+     * @param imageName String input.
+     * @param actionCommand String input.
+     * @param toolTipText String input.
+     */
     public DrawingButton(Class currentClass, ActionListener actionListener, String imageName, String actionCommand, String toolTipText) {
-        String imgLocation = "/icons/"+ imageName + ".png";
+        String imgLocation = "/icons/" + imageName + ".png";
         URL imageURL = currentClass.getResource(imgLocation);
         setFocusable(false);
         setActionCommand(actionCommand);
